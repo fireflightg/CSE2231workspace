db885e622f19659a89d99b9b7d192be7c951a18e
Janaa19 <102869057+Janaa19@users.noreply.github.com>
2023-10-02 21:29:52 -0400

    Update Set3a.java

--- SetOnBST/src/Set3a.java
+++ SetOnBST/src/Set3a.java
@@ -22,7 +22,7 @@ import components.set.SetSecondary;
  * @convention IS_BST($this.tree)
  * @correspondence this = labels($this.tree)
  *
- * @author
+ * @author Jana Abedeljaber, Sonali Manoharan, Saahib Mohammed
  *
  */
 public class Set3a<T extends Comparable<T>> extends SetSecondary<T> {
